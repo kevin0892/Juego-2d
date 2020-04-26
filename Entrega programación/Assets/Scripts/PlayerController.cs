88c01b34d06618b235a99bda10eb347f673054f2
kevin0892 <63006205+kevin0892@users.noreply.github.com>
2020-04-25 19:54:06 -0500

    correccion

--- Entrega programación/Assets/Scripts/PlayerController.cs
+++ Entrega programación/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float fuersaX = 1000;
+    private float fuerzaX = 3000;
     [SerializeField]
-    private float fuerzaSalto = 1000;
+    private float fuerzaSalto = 1880;
     bool canJump;
     // Start is called before the first frame update
     void Start()
@@ -20,11 +20,11 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey("left"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-fuersaX * Time.deltaTime, 0));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-fuerzaX * Time.deltaTime, 0));
         }
         if (Input.GetKey("right")) 
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(fuersaX * Time.deltaTime, 0));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(fuerzaX * Time.deltaTime, 0));
         }
         if (Input.GetKeyDown("up") && canJump)
         {
