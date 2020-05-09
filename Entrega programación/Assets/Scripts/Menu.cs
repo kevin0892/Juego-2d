5aeef308113ac7a911922b72366f4f584ce608e0
kevin0892 <63006205+kevin0892@users.noreply.github.com>
2020-05-08 19:09:30 -0500

    menu listo

---
+++ Entrega programación/Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+  public void CargarEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void CerrarJuego()
+    {
+        Application.Quit();
+        Debug.Log("Salió");
+    }
+}
