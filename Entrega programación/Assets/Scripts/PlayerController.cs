be3a5230b0565518cf139b7de828d0c42e735d0d
kevin0892 <63006205+kevin0892@users.noreply.github.com>
2020-05-11 19:20:51 -0500

    Oui

--- Entrega programación/Assets/Scripts/PlayerController.cs
+++ Entrega programación/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,12 +15,14 @@ public class PlayerController : MonoBehaviour
     private AudioSource jumpSFX, deathSFX;
     private bool canJump;
     private bool canScalate;
-    
+    private Rigidbody2D myRigidbody;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Transform>().position = new Vector3(-38.5f, -17.7f, 0f);
+        myRigidbody = GetComponent<Rigidbody2D>();
+        //gameObject.GetComponent<Transform>().position = new Vector3(-38.5f, -17.7f, 0f);
 
         var parametrosdeEmision = new ParticleSystem.EmitParams();
 
@@ -31,16 +34,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-
         if (Input.GetKey("left"))
         {
             if (canJump == true)
             {
                 GetComponent<ParticleSystem>().Emit(1);
             }
-        
-
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-fuerzaX * Time.deltaTime, 0));
         }
         else if (Input.GetKey("right"))
@@ -53,11 +52,8 @@ public class PlayerController : MonoBehaviour
 
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(fuerzaX * Time.deltaTime, 0));
         }
-             
-         
-        
-        
-        if (Input.GetKeyDown("up") && canJump == true)
+   
+        if (Input.GetKeyDown("z") && canJump == true)
         {
             var parametrosdeEmision = new ParticleSystem.EmitParams();
 
@@ -76,10 +72,6 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-        if (Input.GetKey("up")&& canScalate == true)
-        {
-            transform.position = transform.position+ new Vector3(0,fuerzaEscalada * Time.deltaTime, 0);
-        }
 
     }
 
@@ -91,7 +83,6 @@ public class PlayerController : MonoBehaviour
 
             GetComponent<ParticleSystem>().Emit(4);
         }
-
         if (collision.transform.tag == "trap")
         {
             gameObject.GetComponent<Transform>().position = new Vector3(-38.5f, -17.7f, 0f);
@@ -103,23 +94,49 @@ public class PlayerController : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.transform.tag == "ground" && (collision.transform.tag == "ladder"))
+        {
+            canJump = false;
+
+            GetComponent<ParticleSystem>().Emit(4);
+        }
         if (collision.transform.tag == "ladder")
         {
-            canScalate = true;
-            Debug.Log("si la tocó");
-            GetComponent<Rigidbody2D>() ;
-            var body = GetComponent<Rigidbody2D>();
-            body.mass = 0.01f;
+            myRigidbody.gravityScale = 0;
+            canJump = false;
+        }
+        if (collision.transform.tag == "metas")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-    //te amo
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "ladder")
+        {
+            myRigidbody.gravityScale = 0;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(transform.up * fuerzaEscalada * Time.deltaTime);
+
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(-transform.up * fuerzaEscalada * Time.deltaTime);
+            }
+        }
+    }
     private void OnCollisionExit2D(Collision2D collision)
     {
         canJump = false;
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        canScalate = false;
+        if (collision.transform.tag == "ladder")
+        {
+            myRigidbody.gravityScale = 1;
+        }
+
     }
 
 }
